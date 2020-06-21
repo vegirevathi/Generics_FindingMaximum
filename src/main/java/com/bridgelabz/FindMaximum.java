56e6d0cfbf6d2b18f228a86e9a549d6aef19641d
vegirevathi <vegirevathi@gmail.com>
2020-06-21 20:58:22 +0530

    REFACTOR:Added generic method to find maximum of any data type

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -1,36 +1,15 @@
 package com.bridgelabz;
 
 public class FindMaximum {
-    public Integer analyzeMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        Integer maximum = firstNumber;
-        if (maximum.compareTo(secondNumber) < 0) {
-            maximum = secondNumber;
+    public static <C extends Comparable<C>> C analyzeMaximum(C firstElement, C secondElement, C thirdElement) {
+        C maximum = firstElement;
+        if (maximum.compareTo(secondElement) < 0) {
+            maximum = secondElement;
         }
-        if (maximum.compareTo(thirdNumber) < 0) {
-            maximum = thirdNumber;
+        if (maximum.compareTo(thirdElement) < 0) {
+            maximum = thirdElement;
         }
         return maximum;
-    }
 
-    public Double analyzeFloatMax(double firstNumber, double secondNumber, double thirdNumber) {
-        Double maximum = firstNumber;
-        if (maximum.compareTo(secondNumber) < 0) {
-            maximum = secondNumber;
-        }
-        if (maximum.compareTo(thirdNumber) < 0) {
-            maximum = thirdNumber;
-        }
-        return maximum;
-    }
-
-    public String analyzeStringMax(String firstString, String secondString, String thirdString) {
-        String maximum = firstString;
-        if (maximum.compareTo(secondString) < 0) {
-            maximum = secondString;
-        }
-        if (maximum.compareTo(thirdString) < 0) {
-            maximum = thirdString;
-        }
-        return maximum;
     }
 }
--- src/test/java/com/bridgelabz/findMaximumTest.java
+++ src/test/java/com/bridgelabz/findMaximumTest.java
@@ -29,42 +29,42 @@ public class findMaximumTest {
     @Test
     public void maximumFloat_at_firstPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        Double max = findMaximum.analyzeFloatMax(4.2,2.5,3.8);
+        Double max = findMaximum.analyzeMaximum(4.2,2.5,3.8);
         Assert.assertEquals((Double) 4.2,(Double)max);
     }
 
     @Test
     public void maximumFloat_at_secondPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        Double max = findMaximum.analyzeFloatMax(4.2,5.2,3.8);
-        Assert.assertEquals((Double) 5.2,(Double)max);
+        Double max = findMaximum.analyzeMaximum(2.2,4.2,3.8);
+        Assert.assertEquals((Double) 4.2,(Double)max);
     }
 
     @Test
     public void maximumFloat_at_thirdPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        Double max = findMaximum.analyzeFloatMax(4.2,2.5,7.8);
+        Double max = findMaximum.analyzeMaximum(4.2,2.5,7.8);
         Assert.assertEquals((Double) 7.8,(Double)max);
     }
 
     @Test
     public void maximumCharacter_at_firstPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        String max = findMaximum.analyzeStringMax("Pineapple","Peach", "Banana");
+        String max = findMaximum.analyzeMaximum("Pineapple","Peach", "Banana");
         Assert.assertEquals((String) "Pineapple",(String)max);
     }
 
     @Test
     public void maximumString_at_secondPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        String max = findMaximum.analyzeStringMax("Apple","Cat", "Banana");
+        String max = findMaximum.analyzeMaximum("Apple","Cat", "Banana");
         Assert.assertEquals((String) "Cat",(String)max);
     }
 
     @Test
     public void maximumString_at_thirdPosition() {
         FindMaximum findMaximum = new FindMaximum();
-        String max = findMaximum.analyzeStringMax("Apple","Peach", "Yak");
+        String max = findMaximum.analyzeMaximum("Apple","Peach", "Yak");
         Assert.assertEquals((String) "Yak",(String)max);
     }
 }
