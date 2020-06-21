08b6f09186a7de3a8c10585132aac9639b6bd3f8
vegirevathi <vegirevathi@gmail.com>
2020-06-21 23:11:29 +0530

    ADDED:printMaximum method

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -1,11 +1,15 @@
 package com.bridgelabz;
-
 import java.util.Arrays;
 
+public class FindMaximum<T extends Comparable<T>> {
+    private T[] array;
 
-public class FindMaximum {
-public static <T extends Comparable<T>> T analyzeMaximum(T[] array) {
+    public static <T extends Comparable<T>> T analyzeMaximum(T[] array) {
     Arrays.sort(array);
     return array[array.length-1];
-}
+    }
+
+    public void printMaximum(){
+    System.out.println(array[array.length-1]);
+    }
 }
