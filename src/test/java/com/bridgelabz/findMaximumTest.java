9824bd60204e1e3e14a9b1be134a983774e3f2e7
vegirevathi <vegirevathi@gmail.com>
2020-06-21 13:53:52 +0530

    ADDED:analyzeMaximum method to find maximum of three integers

--- src/test/java/com/bridgelabz/findMaximumTest.java
+++ src/test/java/com/bridgelabz/findMaximumTest.java
@@ -6,9 +6,23 @@ import org.junit.Test;
 public class findMaximumTest {
 
     @Test
-    public void maxInteger_at_firstPosition() {
+    public void maximumInteger_at_firstPosition() {
         FindMaximum findMaximum = new FindMaximum();
         Integer max = findMaximum.analyzeMaximum( 40,20,30 );
         Assert.assertEquals(40,(int)max);
+    }
+
+    @Test
+    public void maximumInteger_at_secondPosition() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer max = findMaximum.analyzeMaximum( 10,40,30 );
+        Assert.assertEquals(40,(int)max);
+    }
+
+    @Test
+    public void maximumInteger_at_thirdPosition() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer max = findMaximum.analyzeMaximum( 10,20,30 );
+        Assert.assertEquals(30,(int)max);
     }
 }
