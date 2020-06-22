35c03450a1ffee20979a48e17896446ffd877190
vegirevathi <vegirevathi@gmail.com>
2020-06-22 11:55:42 +0530

    Modified: print maximum method

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -1,16 +1,16 @@
 package com.bridgelabz;
+
 import java.util.Arrays;
 
-public class FindMaximum<T extends Comparable<T>>{
-    T[] array;
+public class FindMaximum<T> {
 
     public static <T extends Comparable<T>> T analyzeMaximum(T[] array) {
         Arrays.sort(array);
-        return array[array.length-1];
+        return array[array.length - 1];
     }
 
-    public void printMaximum(){
-        System.out.println(array[array.length-1]);
+    public <T> void printMaximum(T[] array) {
+        System.out.println(array[array.length - 1]);
     }
 }
 
--- src/test/java/com/bridgelabz/findMaximumTest.java
+++ src/test/java/com/bridgelabz/findMaximumTest.java
@@ -10,27 +10,25 @@ public class findMaximumTest {
         FindMaximum findMaximum = new FindMaximum();
         Integer[] array = {10, 30, 20, 71, 5, 27, 88, 36};
         Integer max = findMaximum.analyzeMaximum(array);
-        Assert.assertEquals(88,(int)max);
-        //findMaximum.printMaximum();
+        Assert.assertEquals(88, (int) max);
+        findMaximum.printMaximum(array);
     }
-
 
     @Test
     public void maximumFloat_in_array() {
         FindMaximum findMaximum = new FindMaximum();
         Double[] array = {1.2, 2.1, 3.4, 1.5, 7.8, 6.4, 2.89};
         Double max = findMaximum.analyzeMaximum(array);
-        Assert.assertEquals((Double) 7.8,(Double)max);
-        //findMaximum.printMaximum();
+        Assert.assertEquals((Double) 7.8, max);
+        findMaximum.printMaximum(array);
     }
-
 
     @Test
     public void maximumCharacter_in_array() {
         FindMaximum findMaximum = new FindMaximum();
-        String[] array = {"Pineapple","Orange","Apple","Banana"};
+        String[] array = {"Pineapple", "Orange", "Apple", "Banana"};
         String max = findMaximum.analyzeMaximum(array);
-        Assert.assertEquals((String) "Pineapple",(String)max);
-        //findMaximum.printMaximum();
+        Assert.assertEquals((String) "Pineapple", (String) max);
+        findMaximum.printMaximum(array);
     }
 }
